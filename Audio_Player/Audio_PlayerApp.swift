d8a561243e9e5c1cfafd354a2c28e8e8afe0635e
CodeNour <ayman.jibrael@icloud.com>
2022-03-24 13:02:37 +0300

    Initial Commit

---
+++ Audio_Player/Audio_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_PlayerApp.swift
+//  Audio_Player
+//
+//  Created by ayhan on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Audio_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
